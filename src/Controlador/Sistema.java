4377f09a0d06e5be2051d7ea95c5cdce5eb4e9c2
FlyMoon00 <mauriciodrojas00@gmail.com>
2022-09-29 17:16:32 -0300

    Ordene un toque los metodos de sistema

--- src/Controlador/Sistema.java
+++ src/Controlador/Sistema.java
@@ -8,32 +8,33 @@ import java.util.*;
 import Modelos.*;
 
 /**
- * probando
- probando ramassss
- * Prueba de mauricio
- * Ale tu mama no mea
- * @author alejo
+ * Pongan los metodos con los nombres completos, no abrevien palabras
  */
 public class Sistema {
-    private ArrayList<Obra> obras = new ArrayList();
-    private ArrayList<Empresa> empresas = new ArrayList();
-    private ArrayList<Foja_Medicion> fojas = new ArrayList();
-    private ArrayList<Financiacion> finan = new ArrayList();
-    private ArrayList<Certificado> certificados = new ArrayList();
     
-    public void crearEmpresa(Integer cuit, String nombre, String direccion, String repreLegal, String repreTecnico){
-        if(existenciaEmpresa(cuit) != true){
-               Empresa obj = new Empresa(cuit, nombre, direccion, repreLegal, repreTecnico);
-               empresas.add(obj);
-        }else System.out.println("NUMERO DE CUIT: " + cuit + ", YA REGISTRADO!...");
+    private ArrayList <Obra> obras = new ArrayList();
+    private ArrayList <Empresa> empresas = new ArrayList();
+    private ArrayList <Foja_Medicion> fojas = new ArrayList();
+    private ArrayList <Financiacion> finan = new ArrayList();
+    private ArrayList <Certificado> certificados = new ArrayList();
+    
+    //--ItemObra-------------------------------------------------------------------------------------------------
+    
+    public void crearItemObra(String denominacion, int tipo, Integer idObra){
+        Obra obr = buscarObra(idObra);
+        obr.agregarItem(denominacion, tipo);        
     }
     
-    public void crearFinanciacion(String nombre){
-        if ((existenciaFinanciacion(nombre)) != true){
-            Financiacion obj = new Financiacion(ultimoIdFinan() + 1, nombre);
-            finan.add(obj);
-        }else System.out.println("NOMBRE DE FINANCIACION YA EXISTENTE!...");
+    
+    //--CostoItem-------------------------------------------------------------------------------------------------
+    
+    public void crearCostoItem(double monto, Integer inicio_periodo_vigencia, Integer idObra, Integer idItem){
+        Obra obr = buscarObra(idObra);
+        obr.agregarCosto(monto, inicio_periodo_vigencia, idItem);
     }
+    
+    
+    //--Obra-------------------------------------------------------------------------------------------------
     
     public void crearObra(String nombre, float porc_flete, float porc_gastos, float porc_utilidad, float porc_IVA_vivienda, float porc_IVA_infraestructura, String nombre_finan, Integer cuitEmpresa){
         Empresa em = buscarEmpresa(cuitEmpresa);
@@ -41,35 +42,6 @@ public class Sistema {
         Obra ob = new Obra(ultimaObra() + 1, nombre, porc_flete, porc_gastos, porc_utilidad, porc_IVA_vivienda, porc_IVA_infraestructura, em, finan);
         obras.add(ob);
     }
-    
-    public void crearItemObra(String denominacion, int tipo, Integer idObra){
-        Obra obr = buscarObra(idObra);
-        obr.agregarItem(denominacion, tipo);        
-    }
-    
-    public void crearCostoItem(double monto, Integer inicio_periodo_vigencia, Integer idObra, Integer idItem){
-        Obra obr = buscarObra(idObra);
-        obr.agregarCosto(monto, inicio_periodo_vigencia, idItem);
-    }
-   
-    public void getObrasEItems(){
-        for(Obra p : obras){
-            System.out.println(p.getId_Obra() + " " + p.getNombre() + " ");
-            p.getItems();            
-        }
-    }
-    
-    public void getFinanciaciones(){
-        for(Financiacion p : finan){
-            System.out.println(p.getId() + " " + p.getNombre());
-        }
-    }   
-    
-    public void getEmpresas(){
-        for(Empresa p : empresas){
-            System.out.println(p.getCuit() + " " + p.getNombre() + " " + p.getDireccion() + " " + p.getRepreLegal() + " " + p.getRepreTecnico());
-        }
-    }     
     
     private int ultimaObra(){ //busca la ultima obra para hacer autoincremental el id obra.
         int ultimo = obras.size() - 1;
@@ -88,22 +60,21 @@ public class Sistema {
         return null;
     }
     
-    private Integer ultimoIdFinan(){ //busca el ultimo id de financiacion para sumarle 1 y asignarle eso al proximo financiacion a insertar       
-        int ultimo = finan.size() - 1;
-        if (ultimo >= 0) {
-            Financiacion ultimoObj = finan.get(ultimo);
-            return ultimoObj.getId();
-        }else{
-            return 0;
-        }        
-    }
-     
-    private Empresa buscarEmpresa(Integer cuit){ //verifica si existe una empresa o no.
-        boolean existencia = false;
-        for(Empresa p : empresas){
-            if(((p.getCuit()).equals(cuit))) return p; 
+    public void getObrasEItems(){
+        for(Obra p : obras){
+            System.out.println(p.getId_Obra() + " " + p.getNombre() + " ");
+            p.getItems();            
         }
-        return null;
+    }
+    
+    
+    //--Empresa-------------------------------------------------------------------------------------------------
+    
+    public void crearEmpresa(Integer cuit, String nombre, String direccion, String repreLegal, String repreTecnico){
+        if(existenciaEmpresa(cuit) != true){
+               Empresa obj = new Empresa(cuit, nombre, direccion, repreLegal, repreTecnico);
+               empresas.add(obj);
+        }else System.out.println("NUMERO DE CUIT: " + cuit + ", YA REGISTRADO!...");
     }
     
     private boolean existenciaEmpresa(Integer cuit){ //verifica si existe una empresa o no.
@@ -113,6 +84,31 @@ public class Sistema {
         }
         return existencia;
     }
+    
+    private Empresa buscarEmpresa(Integer cuit){ //verifica si existe una empresa o no.
+        boolean existencia = false;
+        for(Empresa p : empresas){
+            if(((p.getCuit()).equals(cuit))) return p; 
+        }
+        return null;
+    }
+    
+    public void getEmpresas(){
+        for(Empresa p : empresas){
+            System.out.println(p.getCuit() + " " + p.getNombre() + " " + p.getDireccion() + " " + p.getRepreLegal() + " " + p.getRepreTecnico());
+        }
+    }     
+    
+    
+    //--Financiacion-------------------------------------------------------------------------------------------------
+    
+    public void crearFinanciacion(String nombre){
+        if ((existenciaFinanciacion(nombre)) != true){
+            Financiacion obj = new Financiacion(ultimoIdFinanciacion() + 1, nombre);
+            finan.add(obj);
+        }else System.out.println("NOMBRE DE FINANCIACION YA EXISTENTE!...");
+    }
+    
     private boolean existenciaFinanciacion(String nombre){ //verifica si existe una empresa o no.
         boolean existencia = false;
         for(Financiacion p : finan){
@@ -127,5 +123,21 @@ public class Sistema {
         }
         return null;
     }
+    
+    private Integer ultimoIdFinanciacion(){ //busca el ultimo id de financiacion para sumarle 1 y asignarle eso al proximo financiacion a insertar       
+        int ultimo = finan.size() - 1;
+        if (ultimo >= 0) {
+            Financiacion ultimoObj = finan.get(ultimo);
+            return ultimoObj.getId();
+        }else{
+            return 0;
+        }        
+    }
+    
+    public void getFinanciaciones(){
+        for(Financiacion p : finan){
+            System.out.println(p.getId() + " " + p.getNombre());
+        }
+    }   
     
 }
